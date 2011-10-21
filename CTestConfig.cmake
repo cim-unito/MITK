a38b4bc1c69b87dd0057fc3e4ef860e59e92ee7d
Miklos Espak <espakm@gmail.com>
2011-10-21 20:11:02 +0200

    CTest config file removed
    CTest config file removed from the repository
    and added to ignore list

--- CTestConfig.cmake
+++
@@ -1,8 +0,0 @@
-set(CTEST_PROJECT_NAME_SUPERBUILD "MITK-superbuild")
-set(CTEST_PROJECT_NAME "MITK")
-set(CTEST_NIGHTLY_START_TIME "22:00:00 EST")
-
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "miles.biotec.unito.it")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=MITK")
-set(CTEST_DROP_SITE_CDASH TRUE)
